0b637432d53fea2d1e0a1d6d4d8b71efcdfee978
leiton-cr <80017848+leiton-cr@users.noreply.github.com>
2022-05-13 19:34:36 -0600

    Particulas para el login
    Agrego componente de partículas

--- Frontend/src/app/shared/shared.module.ts
+++ Frontend/src/app/shared/shared.module.ts
@@ -2,20 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ToggleColorComponent } from './components/toggle-color/toggle-color.component';
 import { LoadingComponent } from './components/loading/loading.component';
-
-
+import { ParticlesComponent } from './components/particles/particles.component';
 
 @NgModule({
   declarations: [
     ToggleColorComponent,
-    LoadingComponent
+    LoadingComponent,
+    ParticlesComponent
   ],
   imports: [
     CommonModule
   ], 
   exports: [
     ToggleColorComponent,
-    LoadingComponent
+    LoadingComponent,
+    ParticlesComponent
   ]
 })
 export class SharedModule { }
