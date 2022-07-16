e3d4ab8b72218f17576418a0931bbf189e4f7d6a
Jennifer <jennifer123mg@gmail.com>
2022-07-16 08:38:35 -0600

    esto

--- Frontend/src/app/modules/cubo/pages/main/main.component.ts
+++ Frontend/src/app/modules/cubo/pages/main/main.component.ts
@@ -2,7 +2,7 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-main',
-  templateUrl: './main.component.html',
+  templateUrl: './main.componet.html',
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent implements OnInit {
